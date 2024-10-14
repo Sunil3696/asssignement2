ea95567b287cdb1b31a5c4dac4704309948750b4
sunil3696 <sunilstark3696@gmail.com>
2024-10-14 00:00:03 -0400

    aa

--- index.js
+++ index.js
@@ -49,6 +49,6 @@ app.use('/api/user', authRoutes);
 const PORT = process.env.PORT || 3000;
 
 // Start the server and listen on the defined port
-app.listen(port, ()=>{
-    console.log(`Server is running on http://localhost:${port}`)
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)
 });
